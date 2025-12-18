b02533aa2802322a6fef6ba4b04e5ef84af09216
Jeremy <pawjwp@users.noreply.github.com>
2025-12-17 23:24:45 -0500

    Added "broken" property to crook to fix predicate
    Registered a model property/predicate for the crook based on the "tic_broken" nbt data, allowing the crook to switch to its broken model when it breaks

--- kubejs/startup_scripts/custom_items/custom_tinker_tools.js
+++ kubejs/startup_scripts/custom_items/custom_tinker_tools.js
@@ -1,10 +1,24 @@
 const $ItemProperties = Java.loadClass("net.minecraft.world.item.Item$Properties");
 const $ModifiableItem = Java.loadClass("slimeknights.tconstruct.library.tools.item.ModifiableItem");
 const $ToolDefinition = Java.loadClass("slimeknights.tconstruct.library.tools.definition.ToolDefinition");
+const ResourceLocation = Java.loadClass("net.minecraft.resources.ResourceLocation");
 
 StartupEvents.registry("item", (event) => {
   event.createCustom(
     "kubejs:crook",
     () => new $ModifiableItem(new $ItemProperties().stacksTo(1), $ToolDefinition.create("kubejs:crook"))
+  );
+});
+
+ItemEvents.modelProperties((event) => {
+  event.register(
+    "kubejs:crook",
+    new ResourceLocation("tconstruct:broken"),
+    /** @type {Internal.BooleanItemPropertyFunction} */
+    (stack) => {
+      if (!stack || !stack.nbt) return 0;
+      const isBroken = stack.nbt.contains("tic_broken") && stack.nbt.getBoolean("tic_broken");
+      return isBroken ? 1 : 0;
+    }
   );
 });
