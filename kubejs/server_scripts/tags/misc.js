f9c45fa37c8e716fcc3c8a1ccb5689ef489a6747
Jeremy <pawjwp@users.noreply.github.com>
2026-02-17 14:51:07 -0500

    Updated city crate chests
    Added new options to the crate loot table and adjusted rates

--- kubejs/server_scripts/tags/misc.js
+++ kubejs/server_scripts/tags/misc.js
@@ -36,6 +36,28 @@ ServerEvents.tags('item', event => {
         'pneumaticcraft:failed_pcb',
     )
 
+    event.add('desolate_planet:lootable_crop_seeds',
+        '#forge:seeds',
+        'scarcity:bamboo_seeds',
+        'scarcity:cactus_seeds',
+        'scarcity:sugar_cane_seeds',
+        'scarcity:sweet_berry_seeds',
+        'scarcity:potato_seeds',
+        'scarcity:carrot_seeds',
+        'scarcity:onion_seeds'
+    )
+
+    event.add('desolate_planet:lootable_tree_seeds',
+        'scarcity:oak_seed',
+        'scarcity:spruce_seed',
+        'scarcity:birch_seed',
+        'scarcity:jungle_seed',
+        'scarcity:acacia_seed',
+        'scarcity:dark_oak_seed',
+        'scarcity:cherry_seed',
+        'scarcity:rubberwood_seed'
+    )
+
     Ingredient.of(/^immersive_weathering:.*_leaf_pile$/).itemIds.forEach(id => {
         event.add('immersive_weathering:leaf_piles', id);
     });
