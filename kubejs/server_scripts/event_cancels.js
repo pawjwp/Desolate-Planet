6c6dad0543787b4ea988a7aa176be2a6fac5c9d7
Jeremy <pawjwp@users.noreply.github.com>
2026-03-10 22:39:47 -0400

    Moved right click denial into KubeJS script

---
+++ kubejs/server_scripts/event_cancels.js
@@ -0,0 +1,11 @@
+BlockEvents.rightClicked(event => {
+    const { player, block, item } = event;
+
+    // Deny right-clicking water with a bowl or terracotta bowl
+    if (block.id === 'minecraft:water') {
+        if (item.id === 'minecraft:bowl' || item.id === 'thirst:terracotta_bowl') {
+            event.cancel();
+            return;
+        }
+    }
+});
