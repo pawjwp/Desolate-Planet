55127736f12fec99e0413e69668979d71907f0b5
Jeremy <pawjwp@users.noreply.github.com>
2026-05-15 14:41:03 -0400

    Blazing blood now converts blocks to glass
    Blazing blood now converts adjacent sand and dust into glass

--- kubejs/server_scripts/tags/misc.js
+++ kubejs/server_scripts/tags/misc.js
@@ -42,6 +42,11 @@ ServerEvents.tags('item', event => {
         'pneumaticcraft:failed_pcb',
     )
 
+    event.add('desolate_planet:superheats_to_glass',
+        '#minecraft:sand',
+        'exdeorum:dust'
+    )
+
     event.add('desolate_planet:lootable_crop_seeds',
         '#forge:seeds',
         'scarcity:bamboo_seeds',
@@ -83,5 +88,10 @@ ServerEvents.tags('block', event => {
         'exdeorum:dust',
         'minecraft:gravel',
         'minecraft:sand',
+    )
+
+    event.add('desolate_planet:superheats_to_glass',
+        '#minecraft:sand',
+        'exdeorum:dust'
     )
 });
